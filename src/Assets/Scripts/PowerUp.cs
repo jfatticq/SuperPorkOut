7f7e160b45bb21487e2e8f33cf22125b45adb31b
Jeff Fattic <jfattic@live.com>
2026-02-04 20:24:26 -0600

    trying to address errors and create a better merge back to main

--- src/Assets/Scripts/PowerUp.cs
+++ src/Assets/Scripts/PowerUp.cs
@@ -1,9 +1,7 @@
-using System.Collections;
 using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
     [Header("Speed Boost")]
     public float boostMultiplier = 1.5f;
-    
 }
--- src/Assets/Scripts/Stamina.cs
+++ src/Assets/Scripts/Stamina.cs
@@ -4,25 +4,14 @@ using UnityEngine.UI;
 public class Stamina : MonoBehaviour
 {
     public float maxStamina = 100f;
-    public float currentStamina;
+    public float currentStamina = 100f;
 
     public float drainRate = 20f;
-   
-
-    void Start()
-    {
-        currentStamina = maxStamina;
-    }
 
     void Update()
     {
+        currentStamina -= drainRate * Time.deltaTime;
         
-       
-            currentStamina -= drainRate * Time.deltaTime;
-        
-       
-
         currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
-        
     }
 }
