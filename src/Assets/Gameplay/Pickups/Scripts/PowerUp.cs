4eb435b39f84931230167033a0b7f43c815b10e8
Jeff Fattic <jfattic@live.com>
2026-02-07 16:56:42 -0600

    pickup fixes
    - added audio clip to each pickup
    - fixed trigger on cabbage pickup
    - added glow and explosion to cabbage and tomato (though tomato glow isn't showing)

---
+++ src/Assets/Gameplay/Pickups/Scripts/PowerUp.cs
@@ -0,0 +1,30 @@
+using SuperPorkOut.Characters.Player;
+using UnityEngine;
+
+namespace SuperPorkOut.Gameplay.Pickups
+{
+    [RequireComponent(typeof(Collider))]
+    public class PowerUp : MonoBehaviour, IPlayerTriggerInteractable
+    {
+        [SerializeField, Min(0f)] private float staminaAmount = 15f;
+
+        [SerializeField] private AudioClip pickupSfx;
+
+        private void Reset()
+        {
+            GetComponent<Collider>().isTrigger = true;
+        }
+
+        public void OnPlayerEnter(PlayerFacade player)
+        {
+            player.Stamina.Add(staminaAmount);
+
+            if (pickupSfx != null)
+                AudioSource.PlayClipAtPoint(pickupSfx, transform.position);
+
+            Destroy(gameObject);
+        }
+
+        public void OnPlayerExit(PlayerFacade player) { }
+    }
+}
