e0f9809cbae92c4076133e279652a4acdd60868c
Markus <mr819@lindenwood.edu>
2026-02-08 12:23:18 -0600

    some

---
+++ src/Assets/Scripts/Stamina.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class Stamina : MonoBehaviour
+{
+    internal float Normalized()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
